00d841c79ca8dc9a96d6990820342176c0f60f94
varunkusabi <100030378+varunkusabi@users.noreply.github.com>
2026-08-03 10:19:55 +0530

    Add: max-area-of-island - submission-0

---
+++ Data Structures & Algorithms/max-area-of-island/submission-0.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int ans = 0;
+        int maxArea = 0;
+        Queue<int[]> q = new LinkedList<>();
+        int[][] vis = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1 && vis[i][j] == 0) {
+                    q.add(new int[] {i, j});
+                    vis[i][j] = 1;
+                    ans=1;
+                    while (!q.isEmpty()) {
+                        int[] currentPair = q.poll();
+                        int x = currentPair[0];
+                        int y = currentPair[1];
+                        if (x + 1 < n && grid[x + 1][y] == 1 && vis[x + 1][y] == 0) {
+                            vis[x + 1][y] = 1;
+                            q.add(new int[] {x + 1, y});
+                            ans++;
+                        }
+                        if (x - 1 >= 0 && grid[x - 1][y] == 1 && vis[x - 1][y] == 0) {
+                            vis[x - 1][y] = 1;
+                            q.add(new int[] {x - 1, y});
+                            ans++;
+                        }
+                        if (y + 1 < m && grid[x][y + 1] == 1 && vis[x][y + 1] == 0) {
+                            vis[x][y + 1] = 1;
+                            q.add(new int[] {x, y + 1});
+                            ans++;
+                        }
+                        if (y - 1 >= 0 && grid[x][y - 1] == 1 && vis[x][y - 1] == 0) {
+                            vis[x][y - 1] = 1;
+                            q.add(new int[] {x, y - 1});
+                            ans++;
+                        }
+                    }
+                }
+                maxArea = Math.max(ans,maxArea);
+                ans=0;
+            }
+        }
+        return maxArea;
+    }
+}
